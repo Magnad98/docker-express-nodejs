eebb84de9dabd88ac88f4426bc43d4ce9f29b259
alexandrupp <39691606+Magnad98@users.noreply.github.com>
2022-01-30 12:57:38 +0200

    Setting up Router & First Endpoint - GET method

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 
+const router = require('./src/routes')
+
 const {json, urlencoded} = express
-
 const app = express()
-
 const port = process.env.PORT || 8080
+
+app.use(json())
+app.use(urlencoded({extended: false}))
 
 const corsOptions = {
     origin: '*',
@@ -14,6 +17,8 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
+
+app.use(router)
 
 app.use('/', (req, res) => {
     res.send('This is the microservice 1 and version v1.0.0')
