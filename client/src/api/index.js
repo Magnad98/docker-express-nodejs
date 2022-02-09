18c7ea51a45abfc9c89715b9b9b9e38038a05676
alexandrupp <39691606+Magnad98@users.noreply.github.com>
2022-02-09 20:21:44 +0200

    Reordering apis. Creating db.json

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,10 +5,10 @@ const api = axios.create({
 })
 
 export const insertMovie = payload => api.post(`/movie`, payload)
-export const getAllMovies = () => api.get(`/movies`)
 export const updateMovieById = (id, payload) => api.put(`/movie/${id}`, payload)
 export const deleteMovieById = id => api.delete(`/movie/${id}`)
 export const getMovieById = id => api.get(`/movie/${id}`)
+export const getAllMovies = () => api.get(`/movies`)
 
 const apis = {
     insertMovie,
