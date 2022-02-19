69a3db57a0e810e8e9f508fceb2ebfaa385bd050
alexandrupp <39691606+Magnad98@users.noreply.github.com>
2022-02-19 18:47:35 +0200

    Created auth API

---
+++ server-auth/index.js
@@ -0,0 +1,19 @@
+let express = require('express');
+let app = express();
+
+let bodyparser = require('body-parser');
+app.use(bodyparser.json());
+
+let mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/cinema');
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB @ 27017');
+});
+  
+const user = require('./route/user-router');
+app.use('/api/user', user);
+  
+const port = 5001;
+app.listen(port, () => {
+    console.log("Server running at port: " + port);
+});
---
+++ server-auth/route/user-router.js
@@ -0,0 +1,89 @@
+const router = require('express').Router(); 
+const User = require('../model/user-model');
+const emailValaidator = require("email-validator");
+const PasswordValidator = require('password-validator');
+const passwordValidator = new PasswordValidator().is().min(8).has().uppercase().has().lowercase().has().digits(1).has().symbols(1)
+
+router.post('/login', (req, res) => { 
+
+    if (!emailValaidator.validate(req.body.email)) {
+        return res.status(400).send({
+            message : "Invalid email"
+        });
+    } else if (!passwordValidator.validate(req.body.password)) {
+        return res.status(400).send({
+            message : "Invalid password"
+        });
+    }
+
+    User.findOne({ email : req.body.email }, function(err, user) {
+        if (user === null) {
+            return res.status(403).send({
+                message : "User not found"
+            });
+        }
+        else {
+            if (!user.validPassword(req.body.password)) {
+                return res.status(403).send({
+                    message : "Wrong password"
+                });
+            } 
+            else {
+                user.generateToken()
+                user.save((err, User) => { 
+                    if (err) { 
+                        return res.status(400).send({ 
+                            message : "Failed to create token"
+                        }); 
+                    } else {
+                        return res.status(201).send({
+                            message : "User Logged In",
+                            token : user.token
+                        })
+                    }
+                });
+            }
+        }
+    });
+}); 
+
+router.post('/register', (req, res) => { 
+
+    if (!emailValaidator.validate(req.body.email)) {
+        return res.status(400).send({
+            message : "Invalid email"
+        });
+    } else if (!passwordValidator.validate(req.body.password)) {
+        return res.status(400).send({
+            message : "Invalid password"
+        });
+    }
+
+    User.findOne({ email : req.body.email }, function(err, user) {
+        if (user !== null) {
+            return res.status(403).send({
+                message : "A user with this email already exists"
+            });
+        } else {
+            let newUser = new User();
+            newUser.email = req.body.email,
+            newUser.password=req.body.password 
+            newUser.setPassword(req.body.password); 
+
+            newUser.save((err, User) => { 
+                if (err) { 
+                    return res.status(400).send({ 
+                        message : "Failed to add user."
+                    }); 
+                } 
+                else { 
+                    return res.status(201).send({ 
+                        message : "User created."
+                    }); 
+                } 
+            });
+        }
+    });
+}); 
+ 
+module.exports = router; 
